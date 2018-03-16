c91981b1aa2a9dbd63f8a3c031861fbddd780562
Kevin <616651408@qq.com>
2018-03-17 03:11:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-echo "Hello World !"
-echo "Hello World !"
-echo "Hello World !"
-echo "Hello World !"
-echo "Hello World !"
-echo "Hello World !"
-echo "Hello World !"
-echo "Hello World !"
 cd /root/lovedev
 npm install
 npm run start
